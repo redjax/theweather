00a530f8d2c280965f5d5c5bdeabc72956ec48a1
redjax <5534031+redjax@users.noreply.github.com>
2025-09-21 18:16:37 -0400

    fix(tree): Add script to print repo tree

---
+++ scripts/repo_tree.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT=$( dirname "$THIS_DIR" )
+
+IGNORE_STR=".git|.venv|__pycache__|.nox|.ruff_cache|.cache"
+
+if ! command -v tree > /dev/null; then
+    echo "tree not installed"
+    exit 1
+fi
+
+tree "$REPO_ROOT" -r -a -f -I "$IGNORE_STR"
