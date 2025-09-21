838bee95bc69bf4ce1caf8e2aafa98a8faec48dd
redjax <5534031+redjax@users.noreply.github.com>
2025-09-21 15:47:26 -0400

    fix(dev): Add dev-env nox session

--- noxfile.py
+++ noxfile.py
@@ -36,16 +36,6 @@ DEFAULT_PYTHON: str = f"{PY_VER_TUPLE[0]}.{PY_VER_TUPLE[1]}"
 # the `resolve()` makes it portable
 VENV_DIR = Path("./.venv").resolve()
 
-LINT_PATHS: list[str] = [
-    "shared",
-    "scripts",
-    "database",
-    "data-pipeline",
-    "collectors",
-    "api-server",
-    ".sandbox",
-]
-
 ## Paths to sub-apps with their own uv project
 UV_PARENT_DIRS = [
     "collectors/openmeteo-collector/",
@@ -55,14 +45,18 @@ UV_PARENT_DIRS = [
 ]
 
 
-def install_uv_project(session: nox.Session, external: bool = False) -> None:
+def install_uv_project(
+    session: nox.Session, external: bool = False, pip_install: bool = False
+) -> None:
     """Method to install uv and the current project in a nox session."""
     log.info("Installing uv in session")
     session.install("uv")
     log.info("Syncing uv project")
     session.run("uv", "sync", external=external)
-    log.info("Installing project")
-    session.run("uv", "pip", "install", ".", external=external)
+
+    if pip_install:
+        log.info("Installing project")
+        session.run("uv", "pip", "install", ".", external=external)
 
 
 @contextmanager
@@ -82,73 +76,52 @@ def dev(session: nox.Session) -> None:
 
     Run this on a fresh clone of the repository to automate building the project with uv.
     """
+    ## Install apps
+    with cd("shared"):
+        log.info("Installing shared/ app")
+        install_uv_project(session, external=True, pip_install=False)
+
+    with cd("collectors/weatherapi-collector/"):
+        log.info("Installing weatherapi-collector/ app")
+        install_uv_project(session, external=True, pip_install=False)
+
+    # with cd("collectors/openmeteo-collector/"):
+    #     log.info("Installing openmeteo-collector/ app")
+    #     install_uv_project(session, external=True, pip_install=False)
+
+    # with cd("api-server/"):
+    #     log.info("Installing api-server/ app")
+    #     install_uv_project(session, external=True, pip_install=False)
+
     ## Setup root project
     install_uv_project(session, external=True)
 
-    for app_parent_dir in UV_PARENT_DIRS:
-        with cd(app_parent_dir):
-            log.info(f"Installing project: {app_parent_dir}")
-            install_uv_project(session, external=True)
-
 
 @nox.session(python=[DEFAULT_PYTHON], name="ruff-lint", tags=["ruff", "clean", "lint"])
-def run_linter(session: nox.Session, lint_paths: list[str] = LINT_PATHS):
+def run_linter(session: nox.Session):
     """Nox session to run Ruff code linting."""
-    if not Path("ruff.toml").exists():
-        if not Path("pyproject.toml").exists():
-            log.warning(
-                """No ruff.toml file found. Make sure your pyproject.toml has a [tool.ruff] section!
+    ## Base paths - adjust as necessary: root is current working directory
+    root = Path(".").resolve()
 
-If your pyproject.toml does not have a [tool.ruff] section, ruff's defaults will be used.
-Double check imports in _init_.py files, ruff removes unused imports by default.
-"""
-            )
+    ## Collect workspace member directories:
+    #  This will match your uv workspace globs
+    members = (
+        list(root.glob("collectors/*")) + [root / "shared"] + [root / "api-server"]
+    )
 
     session.install("ruff")
 
-    log.info("Linting code")
-    for d in lint_paths:
-        if not Path(d).exists():
-            log.warning(f"Skipping lint path '{d}', could not find path")
-            pass
-        else:
-            lint_path: Path = Path(d)
-            log.info(f"Running ruff imports sort on '{d}'")
-            session.run(
-                "ruff",
-                "check",
-                lint_path,
-                "--select",
-                "I",
-                "--fix",
-            )
+    ## Iterate over sub-apps & lint
+    for member_path in members:
+        if not member_path.exists():
+            session.log(f"Skipping non-existent path: {member_path}")
+            continue
 
-            log.info(f"Running ruff checks on '{d}' with --fix")
-            session.run(
-                "ruff",
-                "check",
-                lint_path,
-                "--fix",
-            )
-
-    # log.info("Linting noxfile.py")
-    # session.run(
-    #     "ruff",
-    #     "check",
-    #     f"{Path('./noxfile.py')}",
-    #     "--fix",
-    # )
-
-    ## Find stray Python files not in src/, .venv/, or .nox/
-    all_python_files = [
-        f
-        for f in Path("./").rglob("*.py")
-        if ".venv" not in f.parts
-        and "migrations" not in f.parts
-        and ".nox" not in f.parts
-        and "src" not in f.parts
-    ]
-    log.info(f"Found [{len(all_python_files)}] Python file(s) to lint")
-    for py_file in all_python_files:
-        log.info(f"Linting Python file: {py_file}")
-        session.run("ruff", "check", str(py_file), "--fix")
+        session.log(f"Running ruff lint on {member_path}")
+        session.run(
+            "ruff",
+            "check",
+            str(member_path),
+            "--fix",
+            external=True,
+        )
