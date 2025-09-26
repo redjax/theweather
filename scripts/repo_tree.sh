fa4b888020a166de0cbbf8fa465ec8dbe5c57f76
redjax <5534031+redjax@users.noreply.github.com>
2025-09-25 23:26:21 -0400

    repo(script): Update repo tree script ignore patterns

--- scripts/repo_tree.sh
+++ scripts/repo_tree.sh
@@ -3,7 +3,7 @@
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$( dirname "$THIS_DIR" )
 
-IGNORE_STR=".git|.venv|__pycache__|.nox|.ruff_cache|.cache"
+IGNORE_STR=".git|.venv|__pycache__|.nox|.ruff_cache|.cache|.direnv|.db|.python-version|*.log|*.log.*"
 
 if ! command -v tree > /dev/null; then
     echo "tree not installed"
