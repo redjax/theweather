9f5b25e7e9b6626242accb90dff9cf7504fbf7d7
redjax <5534031+redjax@users.noreply.github.com>
2025-09-28 19:26:08 -0400

    fix(refactor): Refactor main function to determine which scheduler to use

--- collectors/weatherapi-collector/src/weatherapi_collector/__main__.py
+++ collectors/weatherapi-collector/src/weatherapi_collector/__main__.py
@@ -51,79 +51,36 @@ def collect(location_name: str | None = None, forecast_days: int = 1) -> dict:
     return {"current_weather": current_weather, "weather_forecast": weather_forecast}
 
 
-def main():
-    WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST = ["00", "15", "30", "45"]
-    DATA_JOBS_SCHEDULE_MINUTES_LIST: list[str] = ["00", "20", "40"]
-    CLEANUP_JOBS_SCHEDULE_MINUTES_LIST: list[str] = [
-        "00",
-        "05",
-        "10",
-        "15",
-        "20",
-        "25",
-        "30",
-        "35",
-        "40",
-        "45",
-        "50",
-        "55",
-    ]
-
-    RUN_SCHEDULE: bool = WEATHERAPI_SETTINGS.get("RUN_SCHEDULER")
-    SAVE_TO_DB: bool = WEATHERAPI_SETTINGS.get("SAVE_TO_DB")
-    DB_ECHO: bool = WEATHERAPI_SETTINGS.get("DB_ECHO")
-    log.debug(f"Running on schedule: {RUN_SCHEDULE}")
-    log.debug(f"Save responses to DB: {SAVE_TO_DB}, DB echo: {DB_ECHO}")
-
-    initialize_database()
-
-    ## Override schedules (for debugging/dev)
-    SCHEDULE_OVERRIDES = {
-        "WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST": [],
-        "DATA_JOBS_SCHEDULE_MINUTES_LIST": [],
-        "CLEANUP_JOBS_SCHEDULE_MINUTES_LIST": [],
-    }
-
-    ## Override weather api jobs schedule
-    if SCHEDULE_OVERRIDES.get("WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST"):
-        WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST = (
-            WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST
-            + SCHEDULE_OVERRIDES.get("WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST")
-        )
-
-    ## Override data jobs schedule
-    if SCHEDULE_OVERRIDES.get("DATA_JOBS_SCHEDULE_MINUTES_LIST"):
-        DATA_JOBS_SCHEDULE_MINUTES_LIST = (
-            DATA_JOBS_SCHEDULE_MINUTES_LIST
-            + SCHEDULE_OVERRIDES.get("DATA_JOBS_SCHEDULE_MINUTES_LIST")
-        )
-
-    ## Override cleanup jobs schedule
-    if SCHEDULE_OVERRIDES.get("CLEANUP_JOBS_SCHEDULE_MINUTES_LIST"):
-        CLEANUP_JOBS_SCHEDULE_MINUTES_LIST = (
-            CLEANUP_JOBS_SCHEDULE_MINUTES_LIST
-            + SCHEDULE_OVERRIDES.get("CLEANUP_JOBS_SCHEDULE_MINUTES_LIST")
-        )
+def start_schedule_lib_schedules(
+    run_schedule: bool = False,
+    save_to_db: bool = False,
+    db_echo: bool = False,
+    weatherapi_jobs_schedule_minutes_list: list[str] = [],
+    data_jobs_minutes_schedule: list[str] = [],
+    cleanup_jobs_minutes_schedule: list[str] = [],
+):
 
     ## Initialize database engine
     db_engine = None
-    if SAVE_TO_DB:
+    if save_to_db:
         db_engine = get_db_engine()
 
-    log.debug(f"Start scheduler: {RUN_SCHEDULE}")
+    log.debug(
+        f"Start scheduler: {run_schedule}, save to DB: {save_to_db}, DB echo: {db_echo}"
+    )
     location_name: str = WEATHERAPI_SETTINGS.get("LOCATION_NAME")
     forecast_days: int = 1
 
-    if RUN_SCHEDULE:
+    if run_schedule:
         start_weatherapi_scheduled_collection(
             location_name=location_name,
             api_key=WEATHERAPI_SETTINGS.get("API_KEY"),
             forecast_days=forecast_days,
-            save_to_db=SAVE_TO_DB,
-            db_echo=DB_ECHO,
-            weatherapi_jobs_minutes_schedule=WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST,
-            data_jobs_minutes_schedule=DATA_JOBS_SCHEDULE_MINUTES_LIST,
-            cleanup_jobs_minutes_schedule=CLEANUP_JOBS_SCHEDULE_MINUTES_LIST,
+            save_to_db=save_to_db,
+            db_echo=db_echo,
+            weatherapi_jobs_minutes_schedule=weatherapi_jobs_schedule_minutes_list,
+            data_jobs_minutes_schedule=data_jobs_minutes_schedule,
+            cleanup_jobs_minutes_schedule=cleanup_jobs_minutes_schedule,
             db_engine=db_engine,
         )
 
@@ -132,7 +89,7 @@ def main():
         try:
             collected_weatherapi_results: dict = collect(location_name, forecast_days)
 
-            if SAVE_TO_DB:
+            if save_to_db:
                 _current = CurrentWeatherJSONIn(
                     collected_weatherapi_results.get("current_weather")
                 )
@@ -155,9 +112,7 @@ def main():
 
                 log.debug(f"Saving forecast to DB")
                 try:
-                    db_client.save_forecast_response(
-                        forecast_schema=_forecast, engine=engine
-                    )
+                    db_client.save_forecast(forecast_schema=_forecast, engine=engine)
                 except Exception as exc:
                     log.error(f"Failed to save forecast to DB: {exc}")
                     raise
@@ -167,11 +122,91 @@ def main():
             return
 
 
+def main(scheduler: str = "schedule_lib"):
+    RUN_SCHEDULE: bool = WEATHERAPI_SETTINGS.get("RUN_SCHEDULER")
+    SAVE_TO_DB: bool = WEATHERAPI_SETTINGS.get("SAVE_TO_DB")
+    DB_ECHO: bool = WEATHERAPI_SETTINGS.get("DB_ECHO")
+    log.debug(f"Running on schedule: {RUN_SCHEDULE}")
+    log.debug(f"Save responses to DB: {SAVE_TO_DB}, DB echo: {DB_ECHO}")
+
+    initialize_database()
+
+    ####################
+    # Schedule library #
+    ####################
+
+    SCHEDULE_LIB_WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST = ["00", "15", "30", "45"]
+    SCHEDULE_LIB_DATA_JOBS_SCHEDULE_MINUTES_LIST: list[str] = ["00", "20", "40"]
+    SCHEDULE_LIB_CLEANUP_JOBS_SCHEDULE_MINUTES_LIST: list[str] = [
+        "00",
+        "05",
+        "10",
+        "15",
+        "20",
+        "25",
+        "30",
+        "35",
+        "40",
+        "45",
+        "50",
+        "55",
+    ]
+
+    ## Override schedules (for debugging/dev)
+    SCHEDULE_LIB_OVERRIDES = {
+        "SCHEDULE_LIB_WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST": [],
+        "SCHEDULE_LIB_DATA_JOBS_SCHEDULE_MINUTES_LIST": [],
+        "SCHEDULE_LIB_CLEANUP_JOBS_SCHEDULE_MINUTES_LIST": [],
+    }
+
+    ## Override weather api jobs schedule
+    if SCHEDULE_LIB_OVERRIDES.get("SCHEDULE_LIB_WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST"):
+        WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST = (
+            WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST
+            + SCHEDULE_LIB_OVERRIDES.get(
+                "SCHEDULE_LIB_WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST"
+            )
+        )
+
+    ## Override data jobs schedule
+    if SCHEDULE_LIB_OVERRIDES.get("SCHEDULE_LIB_DATA_JOBS_SCHEDULE_MINUTES_LIST"):
+        DATA_JOBS_SCHEDULE_MINUTES_LIST = (
+            DATA_JOBS_SCHEDULE_MINUTES_LIST
+            + SCHEDULE_LIB_OVERRIDES.get("SCHEDULE_LIB_DATA_JOBS_SCHEDULE_MINUTES_LIST")
+        )
+
+    ## Override cleanup jobs schedule
+    if SCHEDULE_LIB_OVERRIDES.get("SCHEDULE_LIB_CLEANUP_JOBS_SCHEDULE_MINUTES_LIST"):
+        CLEANUP_JOBS_SCHEDULE_MINUTES_LIST = (
+            CLEANUP_JOBS_SCHEDULE_MINUTES_LIST
+            + SCHEDULE_LIB_OVERRIDES.get(
+                "SCHEDULE_LIB_CLEANUP_JOBS_SCHEDULE_MINUTES_LIST"
+            )
+        )
+
+    match scheduler:
+        case "schedule_lib":
+            start_schedule_lib_schedules(
+                run_schedule=RUN_SCHEDULE,
+                save_to_db=SAVE_TO_DB,
+                db_echo=DB_ECHO,
+                weatherapi_jobs_schedule_minutes_list=SCHEDULE_LIB_WEATHERAPI_JOBS_SCHEDULE_MINUTES_LIST,
+                data_jobs_minutes_schedule=SCHEDULE_LIB_DATA_JOBS_SCHEDULE_MINUTES_LIST,
+                cleanup_jobs_minutes_schedule=SCHEDULE_LIB_CLEANUP_JOBS_SCHEDULE_MINUTES_LIST,
+            )
+        case _:
+            log.error(f"Unknown scheduler: {scheduler}")
+            raise ValueError(f"Unknown scheduler: {scheduler}")
+
+
 if __name__ == "__main__":
     setup_loguru_logging()
 
+    ## schedule_lib, apscheduler_lib
+    SCHEDULER: str = "schedule_lib"
+
     try:
-        main()
+        main(scheduler=SCHEDULER)
     except Exception as exc:
         log.error(f"({type(exc)}) Failed to run WeatherAPI collector: {exc}")
         exit(1)
