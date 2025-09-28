fdb0490160296123d97df764f9dda621b835d0ca
redjax <5534031+redjax@users.noreply.github.com>
2025-09-27 20:49:03 -0400

    fix(temporal): Add temporal worker, activities, & workflows

---
+++ collectors/weatherapi-collector/src/weatherapi_collector/schedules/temporal/activities.py
@@ -0,0 +1,24 @@
+from weatherapi_collector.client import get_current_weather, get_weather_forecast
+
+from temporalio import activity
+from loguru import logger as log
+
+
+__all__ = [
+    "poll_current_weather",
+    "poll_weather_forecast",
+]
+
+
+@activity.defn
+def poll_current_weather(api_key: str, location: str) -> dict:
+    result: dict = get_current_weather(api_key=api_key, location=location)
+
+    return result
+
+
+@activity.defn
+def poll_weather_forecast(api_key: str, location: str, days: int = 1):
+    result: dict = get_weather_forecast(api_key=api_key, location=location, days=days)
+
+    return result
---
+++ collectors/weatherapi-collector/src/weatherapi_collector/temporal_worker.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from shared.setup import setup_loguru_logging
+from weatherapi_collector.config import TEMPORAL_SETTINGS
+from weatherapi_collector.schedules.temporal import worker
+
+from loguru import logger as log
+
+
+__all__ = ["start_worker"]
+
+
+def start_worker():
+    log.debug("DEBUG logging enabled")
+
+    log.debug(f"Temporal settings: {TEMPORAL_SETTINGS}")
+
+    log.info("Starting Temporal worker")
+    try:
+        asyncio.run(worker.main())
+    except Exception as exc:
+        log.error(f"Temporal worker exited unsuccessfuly: ({type(exc).__name__}) {exc}")
+        raise
+    except KeyboardInterrupt:
+        log.warning("Temporal worker operation cancelled by user")
+        return
+
+
+if __name__ == "__main__":
+    setup_loguru_logging()
+
+    try:
+        start_worker()
+    except Exception as exc:
+        log.error(f"Failed while running Temporal worker: ({type(exc).__name__}) {exc}")
