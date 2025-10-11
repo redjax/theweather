d95539831a1b65e20a7e81e7729e88171d87632a
redjax <5534031+redjax@users.noreply.github.com>
2025-10-10 23:01:08 -0400

    repo(script): Update tree command's ignore line

--- scripts/repo_tree.sh
+++ scripts/repo_tree.sh
@@ -3,7 +3,7 @@
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$( dirname "$THIS_DIR" )
 
-IGNORE_STR=".git|.venv|__pycache__|.nox|.ruff_cache|.cache|.direnv|.db|.python-version|*.log|*.log.*"
+IGNORE_STR=".git|.venv|__pycache__|.nox|.ruff_cache|.cache|.direnv|.db|.python-version|*.log|*.log.*|dist|.logs|*.whl|.rar|.zip|.tar.gz"
 
 if ! command -v tree > /dev/null; then
     echo "tree not installed"
